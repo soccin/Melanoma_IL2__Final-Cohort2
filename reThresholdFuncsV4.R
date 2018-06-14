b9c3fed004cb12b555b80bcf18713332e020e7ee
Nicholas D. Socci <soccin@mskcc.org>
2018-06-14 05:40:35 -0400

    checkpoint

---
+++ reThresholdFuncsV4.R
@@ -0,0 +1,99 @@
+getROC <- function(df,markerPos,markerNeg) {
+
+    dt=df %>%
+        filter(Marker %in% c(markerPos,markerNeg)) %>%
+        unite(MarkerValue,Marker,ValueType) %>%
+        select(Sample,SPOT,UUID,MarkerValue,Value) %>%
+        spread(MarkerValue,Value)
+
+    posIntensity=cc(markerPos,"Intensity")
+    posPositive=cc(markerPos,"Positive")
+    negIntensity=cc(markerNeg,"Intensity")
+    negPositive=cc(markerNeg,"Positive")
+
+    dt[["posIntensity"]]=dt[[posIntensity]]
+    dt[["posPositive"]]=dt[[posPositive]]
+    dt[["negIntensity"]]=dt[[negIntensity]]
+    dt[["negPositive"]]=dt[[negPositive]]
+
+    dt %<>%
+        mutate(category=case_when(
+                negPositive==1 ~ "NEG",
+                TRUE ~ "POS"))
+
+    d.roc=roc(dt$category,dt$posIntensity)
+}
+
+getROCMulti <- function(df,markerPos,markerNegsStr,dblPosMarker=NULL) {
+
+    cat("markerNegsStr =",markerNegsStr,"\n")
+    markerNegs=strsplit(markerNegsStr,":")[[1]]
+    dt=df %>%
+        filter(Marker %in% c(markerPos,markerNegs,dblPosMarker)) %>%
+        unite(MarkerValue,Marker,ValueType) %>%
+        select(Sample,SPOT,UUID,MarkerValue,Value) %>%
+        spread(MarkerValue,Value)
+
+    posIntensity=cc(markerPos,"Intensity")
+    posPositive=cc(markerPos,"Positive")
+    negIntensity=cc(markerNegs,"Intensity")
+    negPositive=cc(markerNegs,"Positive")
+
+    dt[["posIntensity"]]=dt[[posIntensity]]
+    dt[["posPositive"]]=dt[[posPositive]]
+    dt[["negIntensity"]]=apply(dt[negIntensity],1,max)
+    dt[["negPositive"]]=ifelse(apply(dt[negPositive]==1,1,any),1,0)
+
+    dt %<>%
+        mutate(category=case_when(
+                negPositive==1 ~ "NEG",
+                TRUE ~ "POS"))
+
+    if(!is.null(dblPosMarker)) {
+        singlePosIdx=dt$category=="POS" & dt[[cc(dblPosMarker,"Positive")]]==0
+        dt$category[singlePosIdx] <- "NEG"
+    }
+
+    d.roc=roc(dt$category,dt$posIntensity)
+
+}
+
+plot.roc.1 <- function(d.roc,markerPos,markerNeg) {
+
+    plot(d.roc,main=paste0(markerPos,", ",markerNeg),asp='s',
+        print.thres=T,print.thres.cex=1.12,
+        print.auc=T,print.auc.cex=1.12)
+
+}
+
+thresholdIntensity <- function(ii,p){sum(c(min(ii[p==1],max(ii)),max(ii[p==0],min(ii))))/2}
+
+getROCStats <- function(sample,spot,markerPos,markerNegs,d.roc) {
+
+    theta1=mean(coords(d.roc, "b",ret="t"))
+    stats=tibble(
+        Sample=sample,
+        Spot=spot,
+        MarkerPos=markerPos,MarkerNeg=markerNeg,
+        auc=as.numeric(auc(d.roc)),
+        thetaOpt=theta1,
+        specOpt=mean(coords(d.roc,theta1,"t","sp")),
+        sensOpt=mean(coords(d.roc,theta1,"t","se")),
+        precOpt=mean(coords(d.roc,theta1,"t","prec")),
+        accOpt=mean(coords(d.roc,theta1,"t","acc")),
+        theta0=asinh(thetas[paste(sample,markerPos,sep=":")]),
+        thetaOrig=thetas[paste(sample,markerPos,sep=":")],
+        spec0=mean(coords(d.roc,theta0,"t","sp")),
+        sens0=mean(coords(d.roc,theta0,"t","se")),
+        prec0=mean(coords(d.roc,theta0,"t","prec")),
+        acc0=mean(coords(d.roc,theta0,"t","acc")),
+        numCases=len(d.roc$cases),
+        numControls=len(d.roc$controls)
+    )
+
+    stats$F1=2*stats$precOpt*stats$sensOpt/(stats$precOpt+stats$sensOpt)
+    stats$F0=2*stats$prec0*stats$sens0/(stats$prec0+stats$sens0)
+
+    return(stats)
+
+}
---
+++ reThresholdV4.R
@@ -0,0 +1,157 @@
+require(tidyr)
+require(tibble)
+require(readr)
+require(ggplot2)
+#require(ggrepel)
+require(yarrr)
+require(pROC)
+
+source("Halo/loadHaloObjectFiles.R")
+
+source("reThresholdFuncsV4.R")
+source("getAllCombinations.R")
+
+markerPos="SOX10"
+
+args <- commandArgs(trailing=T)
+doExclusions=TRUE
+
+if(args[1]=="NULL") {
+    dblPosMarker=NULL
+} else {
+    dblPosMarker=args[1]
+}
+args=args[-1]
+
+i=1
+dd=loadHaloObjFile(args[i],exclude=doExclusions)
+
+oDir=file.path("NewThresholdV3",cc(markerPos,dblPosMarker))
+dir.create(oDir,showWarnings=F)
+
+dx=dd %>%
+    select(Sample,SPOT,UUID,Marker,ValueType,Value) %>%
+    spread(ValueType,Value)
+
+thresholdTbl=dx %>%
+    group_by(Sample,Marker) %>%
+    summarize(Theta=thresholdIntensity(Intensity,Positive)) %>%
+    unite(SampleMarker,Sample,Marker,sep=":",remove=F)
+
+thetas=thresholdTbl$Theta
+names(thetas)=thresholdTbl$SampleMarker
+
+dx$Threshold=thetas[paste(dx$Sample,dx$Marker,sep=":")]
+
+intensityStats=dx %>%
+    group_by(Sample,Marker) %>%
+    summarize(Median=median(Intensity),
+        Mean=mean(Intensity),
+        Max=max(Intensity),
+        MaxN=max(Intensity*(1-Positive)),
+        MinP=min(ifelse(Positive,Intensity,Inf)))
+intensityStats=full_join(thresholdTbl,intensityStats)
+
+studyName="MelanomaIL2_Final_C2"
+
+samples=dx %>% distinct(Sample) %>% pull(Sample)
+
+markersToTest=scan(cc("reThresholdMarkers_",markerPos,".txt"),"")
+
+pltFile=file.path(oDir,cc("thresholdROCCurves",
+    markerPos,
+    paste0(samples,collapse=","),
+    ".pdf"))
+
+if(!interactive()) pdf(file=pltFile,width=8.5,height=11)
+
+omar=par()$mar
+
+roc.stats.all=list()
+
+if(interactive()) stop("BreakPoint-Alpha")
+
+for(sample in samples) {
+    spots=dx %>% filter(Sample==sample) %>% distinct(SPOT) %>% pull(SPOT)
+    for(spot in spots) {
+
+        mInten=dd %>%
+            filter(Sample==sample & SPOT==spot & Marker==markerPos & ValueType=="Intensity") %>%
+            pull(Value)
+        amInten=asinh(mInten)
+        theta0=thetas[grep(markerPos,names(thetas))]
+        xMax=max(max(mInten),theta0)
+
+        cat(sample,",",spot,"\n")
+        tbl=dd %>%
+            filter(Sample==sample & SPOT==spot, ValueType=="Positive") %>%
+            count(Marker,Value) %>%
+            filter(Value==1) %>%
+            arrange(desc(n)) %>%
+            filter(Marker %in% c(markerPos,markersToTest))
+
+        roc.stats=list()
+        newThetas=c()
+        if(interactive()) {
+            par(mfrow=c(3,2))
+        } else {
+            par(mfrow=c(5,2))
+        }
+
+        allMarkerNegCombinations=getAllCombinations(markersToTest)
+        nCombs=len(allMarkerNegCombinations)
+        aucMax=0
+        for(markerNeg in allMarkerNegCombinations) {
+            cat("testing",markerNeg,"  ")
+            dff=dd %>% filter(Sample==sample & SPOT==spot)
+            ss=try({d.roc=getROCMulti(dff,markerPos,markerNeg,dblPosMarker)})
+            if(class(ss)=="roc") {
+                stats=getROCStats(sample,spot,markerPos,markerNeg,d.roc)
+
+                newStatI=len(roc.stats)+1
+                roc.stats[[newStatI]]=stats
+
+                cat("AUC =",stats$auc,"\n")
+
+                # if(!roc.stats[[newStatI]]$thetaOpt %in% newThetas |
+                #     markerNeg==allMarkerNegCombinations[nCombs]) {
+                if(stats$auc>aucMax |
+                    markerNeg==allMarkerNegCombinations[nCombs]) {
+                    aucMax=stats$auc
+                    #newThetas=c(newThetas,roc.stats[[newStatI]]$thetaOpt)
+                    par(pty='s')
+                    plot.roc.1(d.roc,markerPos,markerNeg)
+                    par(pty="m")
+                    plot(density(amInten,from=min(amInten),to=max(amInten)),
+                        main=paste(markerPos,markerNeg,sample,spot),
+                        xlab="asinh(Intensity)", xlim=c(0,asinh(xMax)))
+                    abline(v=asinh(stats$thetaOpt),col="darkgreen",lwd=2,lty=2)
+                    abline(v=asinh(thetas[[paste(sample,markerPos,sep=":")]]),col="darkred",lwd=2,lty=2)
+                    abline(v=mean(amInten),lty=2,lwd=2,col=8)
+                    rug(asinh(d.roc$controls),col="blue",lwd=2)
+                    if(markerNeg==allMarkerNegCombinations[nCombs]) {
+                        thetaOpt <- roc.stats %>%
+                                        bind_rows %>%
+                                        arrange(desc(auc)) %>%
+                                        slice(1) %>%
+                                        pull(thetaOpt)
+                        abline(v=asinh(thetaOpt),col="lightgreen",lwd=2,lty=2)
+                    }
+
+                }
+            }
+        }
+
+        roc.stats.all=c(roc.stats.all,roc.stats)
+
+    }
+}
+
+if(!interactive()) dev.off()
+
+roc.tbl=bind_rows(roc.stats.all)
+write_csv(roc.tbl,
+    file.path(oDir,cc("rocStats",paste0(samples,collapse=","),markerPos,".csv")))
+write_csv(intensityStats,
+    file.path(cc("intensityStats",paste0(samples,collapse=","),markerPos,".csv")))
+
